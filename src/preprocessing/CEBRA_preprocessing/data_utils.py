92d1b825010bd4b1c9e7e668135c62451299034a
riccardoprog <92370991+riccardoprog@users.noreply.github.com>
2024-11-20 17:15:32 +0100

    cleaning of utils

---
+++ src/preprocessing/CEBRA_preprocessing/data_utils.py
@@ -0,0 +1,299 @@
+import pandas as pd
+import os
+import numpy as np
+import torch
+import cebra
+import cebra.datasets
+import copy
+
+
+def multisession_preparation(stimuli: list, split: float):
+    
+    print(type(stimuli))
+    
+    # make sure it's in a list format
+    if isinstance(stimuli,list):
+        print('list of stimuli:', stimuli)
+        #stimuli = set(stimuli)
+    elif isinstance(stimuli,str):
+        stimuli = [stimuli]
+
+    try:
+        data_dir = os.environ["DATA_PATH"]
+    except KeyError:
+        raise ValueError("DATA_PATH environment variable is not set")
+
+
+    data_stimulus_path =  os.path.join(os.environ["DATA_PATH"],'df_stimulus.csv')
+    data_dff_path = os.path.join(os.environ["DATA_PATH"],'dff_trace.npy')
+
+    df_stimulus = pd.read_csv(data_stimulus_path)
+    dff_trace = np.load(data_dff_path)
+
+    data_train = []
+    data_test = []
+    labels_train =[]
+    labels_test = []
+    embeddings_train = []
+    embeddings_test =[]
+    embeddings_simple = []
+
+    print(stimuli)
+
+    for stimulus in stimuli:
+
+        print('Processing stimulus: ',stimulus)
+
+        print('type stimulus', type(stimulus))
+        load_path = os.path.join(os.environ["DATA_PATH"],stimulus,'Dinov2_embeddings/vitb14.pt')
+        print('OS PATH', os.environ["DATA_PATH"])
+        print('load path', load_path)
+        embeddings = torch.load(load_path,map_location=torch.device('cpu'))
+
+        single_stimulus_df = df_stimulus[df_stimulus['stim_type'] == stimulus].reset_index()
+
+        # Create embeddingsExtended to align with filtered_df
+        embeddingsExtended = torch.empty(len(single_stimulus_df), embeddings.size(1))
+
+        # Map the frame numbers to their respective indices in the embeddings tensor
+        for idx, frame in enumerate(single_stimulus_df['frame'].values):
+
+            embeddingsExtended[idx] = embeddings[frame]
+
+        dff_trace_stimulus_all_repetitions = dff_trace[single_stimulus_df['index'].values,:]    
+
+        segments = _get_training_repeat_indices(single_stimulus_df) # list of tuples 
+        
+       #training sets split*10
+        train_idx = int(split*len(segments)) - 1
+        
+        print('TRAIN INDEX', train_idx)
+        print('END TRAIN:' ,segments[train_idx][1])
+        print('BEGIN TEST:' ,segments[train_idx+1][0])
+        print('END TEST: ',segments[-1][1])
+        print('FULL SIZE: ', dff_trace_stimulus_all_repetitions.shape)
+
+        train_dff = dff_trace_stimulus_all_repetitions[segments[0][0]:segments[train_idx][1],:]
+        train_embeddings_stimulus = embeddingsExtended[segments[0][0]:segments[train_idx][1],:]
+        train_labels = single_stimulus_df['frame'].values[segments[0][0]:segments[train_idx][1]]
+
+        test_dff = dff_trace_stimulus_all_repetitions[segments[train_idx+1][0]:segments[-1][1],:]
+        test_embeddings_stimulus = embeddingsExtended[segments[train_idx+1][0]:segments[-1][1],:]
+        test_labels = single_stimulus_df['frame'].values[segments[train_idx+1][0]:segments[-1][1]]
+            
+        ##########################################
+        # Filter out the frame 899
+
+        idx_no_899_train = np.where(train_labels != 899)[0]
+        idx_no_899_test = np.where(test_labels != 899)[0]
+
+        train_labels = train_labels[idx_no_899_train]
+        test_labels = test_labels[idx_no_899_test]
+
+        train_dff = train_dff[idx_no_899_train,:]
+        test_dff = test_dff[idx_no_899_test,:]
+
+        train_embeddings_stimulus = train_embeddings_stimulus[idx_no_899_train,:]
+        test_embeddings_stimulus = test_embeddings_stimulus[idx_no_899_test,:]
+
+        ##########################################
+        data_train.append(train_dff)
+        data_test.append(test_dff)
+
+        labels_train.append(train_labels)
+        labels_test.append(test_labels)
+
+        embeddings_train.append(train_embeddings_stimulus)
+        embeddings_test.append(test_embeddings_stimulus)
+        embeddings_simple.append(embeddings)
+
+    return data_train,data_test,labels_train,labels_test, embeddings_train,embeddings_test,embeddings_simple
+
+
+def _get_training_repeat_indices(df):
+  segments = [] # to store the tuples (start_idx,end_idx)
+
+  start_idx = None
+  for idx, row in df.iterrows():
+      if row['frame'] == 0:
+          if start_idx is not None:
+              # When we find a new 0, the previous segment ends here
+              segments.append((start_idx, idx - 1))
+          start_idx = idx
+
+  # Append the last segment
+  if start_idx is not None:
+      segments.append((start_idx, len(df) - 1))
+
+  return segments
+
+
+def add_function_at_line(file_path, new_function, line_number):
+  #line_number manually found by looking in the files.
+  
+  # Read the existing file content into a list of lines
+  with open(file_path, 'r') as file:
+      lines = file.readlines()
+
+  indent = 4 # manually found in the file
+
+  indented_function = '\n'.join([(' ' * indent) + line for line in new_function.strip().split('\n')])
+
+  # Insert the new function at the desired line number
+  lines.insert(line_number, indented_function + '\n')  # Add a newline at the end
+
+  # Write the modified content back to the file
+  with open(file_path, 'w') as file:
+      file.writelines(lines)
+
+def split_data_HPC(data, test_ratio):
+
+    split_idx = int(len(data)* (1-test_ratio))
+    neural_train = data.neural[:split_idx]
+    neural_test = data.neural[split_idx:]
+    label_train = data.continuous_index[:split_idx]
+    label_test = data.continuous_index[split_idx:]
+
+    return neural_train.numpy(), neural_test.numpy(), label_train.numpy(), label_test.numpy()
+
+def separate_activations(activations):
+    # put everything in a list
+    embeddings_untrained = []
+    embeddings_trained = []
+
+    for key, value in activations.items():
+      if 'UT' in key:
+        embeddings_untrained.append(value.squeeze().cpu().numpy())
+      else:
+        embeddings_trained.append(value.squeeze().cpu().numpy())
+    return embeddings_untrained,embeddings_trained
+
+########################################################################################################################
+########################################################################################################################
+######################################## TAKEN FROM utils_allen.py of Célia ############################################
+########################################################################################################################
+########################################################################################################################
+
+
+def get_single_session_datasets(
+    test_session=9,
+    corrupted=False,
+    pseudomice=False,
+    mice=4,
+    shot_noise: float = None,
+    gaussian_noise: float = None,
+):
+    """
+    Args: 
+        test_session: The session ID to consider as the test session. NOTE(celia): this will need
+            to be changed if we want to test smaller training set number of repeats.
+        corrupted: If True, loads the corrupted dataset, see `datasets/allen/single_session_ca.py` in CEBRA 
+            codebase.
+        pseudomice: If True, uses pseudomice rather than full, with default number of neurons per 
+            mouse.
+        mice: Number of mice to use (max is 4, for now). NOTE(celia): this could be increased by
+            pre-processing more mice. 
+        shot_noise: Level of shot noise (Poisson noise) to apply on the dataset. Default is None, 
+            and that means that no noise is applied.
+        gaussian_noise: Value of the standard deviaiton of the Gaussian noise to add on the data.
+            Default is None, and that means that no noise is applied.
+    
+    Returns: 
+        The train and valid datasets, and the train and valid frame IDs.
+    """
+    train_datas, valid_datas = [], []
+    if corrupted:
+        for i in range(mice):
+            train_datas.append(
+                cebra.datasets.init(
+                    f"allen-movie1-ca-single-session-decoding-corrupt-{i}-repeat-{test_session}-train"
+                )
+            )
+            valid_datas.append(
+                cebra.datasets.init(
+                    f"allen-movie1-ca-single-session-decoding-corrupt-{i}-repeat-{test_session}-test"
+                )
+            )
+    else:
+        for i in range(4):
+            train_datas.append(
+                cebra.datasets.init(
+                    f"allen-movie1-ca-single-session-decoding-{i}-repeat-{test_session}-train"
+                )
+            )
+            valid_datas.append(
+                cebra.datasets.init(
+                    f"allen-movie1-ca-single-session-decoding-{i}-repeat-{test_session}-test"
+                )
+            )
+    if pseudomice:
+        for i in range(len(train_datas)):
+            train_datas[i].neural = torch.from_numpy(
+                obtain_pseudomice(
+                    [train_datas[i].neural for i in range(len(train_datas))]
+                )
+            )
+            valid_datas[i].neural = torch.from_numpy(
+                obtain_pseudomice(
+                    [valid_datas[i].neural for i in range(len(train_datas))]
+                )
+            )
+
+    # Add noise to the first mouse only
+    if shot_noise is not None:
+        # train_datas[0].neural = _add_shot_noise(train_datas[0].neural, scale_factor=shot_noise)
+        valid_datas[0].neural = _add_shot_noise(
+            valid_datas[0].neural, scale_factor=shot_noise
+        )
+    elif gaussian_noise is not None:
+        # train_datas[0].neural = _add_gaussian_noise(train_datas[0].neural, sigma=gaussian_noise)
+        valid_datas[0].neural = _add_gaussian_noise(
+            valid_datas[0].neural, sigma=gaussian_noise
+        )
+
+    # discrete_labels = [np.tile(np.arange(900), 10) for i in range(len(mice))]
+    discrete_labels_train = [np.tile(np.arange(900), 9) for i in range(mice)]
+    discrete_labels_val = [np.tile(np.arange(900), 1) for i in range(mice)]
+
+    return train_datas, valid_datas, discrete_labels_train, discrete_labels_val
+
+
+def obtain_pseudomice(mice, num_neurons_per_mouse=80):
+    """
+    Creates a pseudomouse by selecting a random subset of neurons from each mouse's neural data.
+
+    Parameters:
+    mice (list): List of neural data for different mice
+    num_neurons_per_mouse (int): the number of neurons to select from each mouse
+
+    Returns:
+    Mouse: a pseudomouse object with the concatenated neural data from the selected neurons
+    """
+    neuron_ids = []
+    pseudomice_matrix = None
+    for i, session in enumerate(mice):
+        session_length = session.shape[1]
+        selected_neurons = np.random.choice(
+            session_length, replace=False, size=num_neurons_per_mouse
+        )
+        neuron_ids.append(selected_neurons)
+        if pseudomice_matrix is None:
+            pseudomice_matrix = session[:, selected_neurons]
+        else:
+            pseudomice_matrix = np.concatenate(
+                (pseudomice_matrix, session[:, selected_neurons]), axis=1
+            )
+
+    pseudomouse = copy.deepcopy(mice[0])
+    pseudomouse = pseudomice_matrix
+    return pseudomouse
+
+
+def _add_gaussian_noise(neural_data, sigma: float = 2):
+    gaussian_noise = torch.normal(mean=0.0, std=sigma, size=neural_data.size())
+    return neural_data + gaussian_noise
+
+
+def _add_shot_noise(neural_data, scale_factor: float = 1.0):
+    # Neural data * scale_factor = Poisson lambda
+    return torch.poisson(neural_data * scale_factor) / scale_factor
---
+++ src/preprocessing/CEBRA_preprocessing/plotting_utils.py
@@ -0,0 +1,261 @@
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import numpy as np
+
+
+def plot_hippocampus(ax, embedding, label, gray = False, idx_order = (0,1,2)):
+    r_ind = label[:,1] == 1
+    l_ind = label[:,2] == 1
+
+    if not gray:
+        r_cmap = 'cool'
+        l_cmap = 'magma'
+        r_c = label[r_ind, 0]
+        l_c = label[l_ind, 0]
+    else:
+        r_cmap = None
+        l_cmap = None
+        r_c = 'gray'
+        l_c = 'gray'
+
+    idx1, idx2, idx3 = idx_order
+    r=ax.scatter(embedding [r_ind,idx1],
+               embedding [r_ind,idx2],
+               embedding [r_ind,idx3],
+               c=r_c,
+               cmap=r_cmap, s=0.05, alpha=0.75)
+    l=ax.scatter(embedding [l_ind,idx1],
+               embedding [l_ind,idx2],
+               embedding [l_ind,idx3],
+               c=l_c,
+               cmap=l_cmap, s=0.05, alpha=0.75)
+
+    ax.grid(False)
+    ax.xaxis.pane.fill = False
+    ax.yaxis.pane.fill = False
+    ax.zaxis.pane.fill = False
+    ax.xaxis.pane.set_edgecolor('w')
+    ax.yaxis.pane.set_edgecolor('w')
+    ax.zaxis.pane.set_edgecolor('w')
+
+    return ax
+
+
+def plot_allen(ax, embedding, label, gray = False, idx_order = (0,1,2)):
+    c = label
+
+    idx1, idx2, idx3 = idx_order
+    ax.scatter(embedding [:,idx1],
+               embedding [:,idx2],
+               embedding [:,idx3],
+               c=c,
+               cmap='magma', s=0.05, alpha=0.75)
+
+    ax.grid(False)
+    ax.xaxis.pane.fill = False
+    ax.yaxis.pane.fill = False
+    ax.zaxis.pane.fill = False
+    ax.xaxis.pane.set_edgecolor('w')
+    ax.yaxis.pane.set_edgecolor('w')
+    ax.zaxis.pane.set_edgecolor('w')
+
+    return ax
+
+
+
+def plot_tsne_embeddings_layers(embeddings_tsne_untrained, embeddings_tsne_trained,labels,sample_plot = 100, solver = 'single-session', comparison = 'untrained',data = 'HPC'):
+
+
+    num_layers = len(embeddings_tsne_trained)
+
+    fig, axs = plt.subplots(2, num_layers, figsize=(15, 10), subplot_kw={'projection': '3d'})
+
+    # Flatten the array of axes for easier indexing
+    axs = axs.flatten()
+
+    # Separate the axes into untrained and trained lists
+    axs_untrained = axs[:num_layers]
+    axs_trained = axs[num_layers:]
+
+
+    # Prepare data for embedding, labels, and titles
+    '''labels_list = [label_extended[1:-1], label_extended[2:-2], label_extended[4:-4],
+                  label_extended[5:-5], label_extended[5:-5]]''' # COMPUTE ALL THE EMBEDINGS AFTER
+
+    labels_list = [labels[:sample_plot]] * num_layers
+
+    titles = []
+    if comparison == 'untrained':
+      for layer in range(1, num_layers):
+          titles.append((f'Layer {layer} Untrained', f'Layer {layer} Trained'))
+      titles.append(('Output untrained','Output Trained'))
+    else:
+      for layer in range(1, num_layers):
+          titles.append((f'Layer {layer} Trained', f'Layer {layer} Trained'))
+      titles.append(('Output Single','Output Multi'))
+
+    i = 0
+    for label, ax, ax_trained in zip( labels_list, axs_untrained, axs_trained):
+        embedding = embeddings_tsne_untrained[i][:sample_plot,:]
+        embedding_trained = embeddings_tsne_trained[i][:sample_plot,:]
+        if data == 'HPC':
+          ax = plot_hippocampus(ax, embedding, label)
+        else:
+          ax = plot_allen(ax, embedding, label)
+
+        ax.set_title(titles[i][0], y=1)
+        ax.axis('off')
+        if data =='HPC':
+          ax_trained = plot_hippocampus(ax_trained, embedding_trained, label)
+        else:
+          ax_trained = plot_allen(ax_trained, embedding_trained, label)
+
+        ax_trained.set_title(titles[i][1], y=1)
+        ax_trained.axis('off')
+        i = i + 1
+
+    fig.suptitle(f't-SNE across layers ({solver})', fontsize= 20)
+
+    plt.subplots_adjust(wspace=0,
+                        hspace=0)
+    plt.tight_layout()
+
+    plt.show()
+
+
+def plot_activations(input_data,embeddings_untrained,embeddings_trained,sample_plot = 100, solver = 'single', comparison = 'untrained'):
+    num_layers = len(embeddings_trained)
+
+    # Set up the figure and gridspec layout
+    fig = plt.figure(figsize=(10, 15))
+    fig.suptitle(f'Comparison between layers ({solver})', fontsize=20)
+    gs = gridspec.GridSpec(num_layers+1, 2)  # Create a grid with 5 rows and 2 columns
+
+    # Create a subplot that spans both columns for the top plot
+    ax_top = fig.add_subplot(gs[0, :])  # Top row, spanning both columns
+    ax_top.imshow(input_data.T[:, 0:sample_plot])
+    ax_top.set_title('Input Data')
+    #ax_top.axis('off')
+    ax_top.set_ylabel('Channel #')
+    ax_top.set_xlabel('Time')
+    ax_top.grid(False)
+
+    # Now set up the existing subplots for the comparisons
+    titles = []
+    if comparison == 'untrained':
+      for layer in range(1, num_layers):
+          titles.append(f'Layer {layer} Untrained')
+          titles.append(f'Layer {layer} Trained')
+      titles.append(f'Output Untrained')
+      titles.append(f'Output Trained')
+    else:
+      for layer in range(1, num_layers):
+          titles.append(f'Layer {layer} Single')
+          titles.append(f'Layer {layer} Multi')
+
+      titles.append(f'Output Single')
+      titles.append(f'Output Multi')
+
+    # Create each subplot
+    axes = []
+    for i in range(num_layers):
+        for j in range(2):
+            ax = fig.add_subplot(gs[i + 1, j])  # Shift down by 1 row for each layer
+            axes.append(ax)
+
+    # Interleave untrained and trained embeddings
+    ax_images = [None] * (2 * num_layers)
+    ax_images[::2] =  embeddings_untrained# Place untrained embeddings at even indices
+    ax_images[1::2] =  embeddings_trained # Place trained embeddings at odd indices
+
+    for ax, img, title in zip(axes, ax_images, titles):
+        ax.imshow(img[:, 0:sample_plot], cmap = 'magma')
+        ax.set_title(title)
+        ax.set_ylabel('Unit #')
+        ax.set_xlabel('Time')
+        ax.grid(False)  # Hide gridlines
+        #ax.axis('off')  # Hide axis for the subplots
+
+    # Adjust layout for better spacing
+    plt.tight_layout()
+    plt.show()
+
+
+def plot_cebra_embeddings_layers(embeddings_untrained,embeddings_trained,labels_list, solver, comparison = 'untrained',dataset = 'HPC'):
+
+    num_layers = len(embeddings_trained)
+
+    fig, axs = plt.subplots(2, num_layers, figsize=(15, 10), subplot_kw={'projection': '3d'})
+    fig.suptitle(f'Embeddings across layers ({solver})', fontsize=20)
+
+    # Flatten the array of axes for easier indexing
+    axs = axs.flatten()
+
+    # Separate the axes into untrained and trained lists
+    axs_untrained = axs[:num_layers]
+    axs_trained = axs[num_layers:]
+
+    # Now set up the existing subplots for the comparisons
+    titles = []
+    if comparison == 'untrained':
+      for layer in range(1, num_layers):
+          titles.append((f'Layer {layer} Untrained', f'Layer {layer} Trained'))
+      titles.append(('Output untrained','Output Trained'))
+    else:
+      for layer in range(1, num_layers):
+          titles.append((f'Layer {layer} Single', f'Layer {layer} Multi'))
+      titles.append(('Output Single','Output Multi'))
+
+    i = 0
+    for label, ax, ax_trained in zip(labels_list, axs_untrained, axs_trained):
+        emb_untrained = embeddings_untrained[i].T
+        emb_trained = embeddings_trained[i].T
+        if dataset == 'HPC':
+           ax = plot_hippocampus(ax, emb_untrained, label)
+        else:
+          ax = plot_allen(ax, emb_untrained, label)
+
+        ax.set_title(titles[i][0], y=1, pad=-20)
+        ax.axis('off')
+        ax_trained = plot_allen(ax_trained, emb_trained, label)
+        ax_trained.set_title(titles[i][1], y=1, pad=-20)
+        ax_trained.axis('off')
+        i = i + 1
+
+
+    #plt.subplots_adjust(wspace=0,hspace=0)
+    plt.tight_layout() #pad=0.5
+    plt.show()
+
+
+def plot_rdm(rdms, titles, metric='Normalized Euclidean distance'):
+    # Create the plot
+    fig, ax = plt.subplots(1, 2)
+    fig.set_size_inches(10, 7)
+
+    # Generate tick labels
+    tick_labels = [str(i) for i in range(0, 930, 30)]
+
+    for i, rdm in enumerate(rdms):
+        # Display the RDM using imshow
+        cax = ax[i].imshow(rdm, cmap='viridis', aspect='auto')
+
+        # Set title and show the plot
+        ax[i].set_title(titles[i])
+
+        # Set ticks and tick labels
+        num_ticks = len(tick_labels)
+        ax[i].set_xticks(np.linspace(0, rdm.shape[1] - 1, num_ticks))
+        ax[i].set_yticks(np.linspace(0, rdm.shape[0] - 1, num_ticks))
+        ax[i].set_xticklabels(tick_labels, rotation=90, ha='right')
+        ax[i].set_yticklabels(tick_labels)
+
+    plt.suptitle('Representational Dissimilarity Matrix (RDM)')
+    plt.tight_layout()
+    plt.subplots_adjust(bottom=0.2)
+
+    # Add colorbar with a label
+    fig.colorbar(cax, ax=ax, orientation='horizontal', fraction=0.05, label=metric)
+
+    plt.show()
+
