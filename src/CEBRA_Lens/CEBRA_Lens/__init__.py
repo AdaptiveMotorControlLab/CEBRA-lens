0a86d95563c7c0a906498182b1c495b2bf8c3e6b
Riccardo Carpineto <92370991+riccardoprog@users.noreply.github.com>
2024-12-19 18:51:14 +0100

    Delete src/CEBRA_Lens directory
    very strange on GitHub the folders are not the same

--- src/CEBRA_Lens/CEBRA_Lens/__init__.py
+++
@@ -1,9 +0,0 @@
-# example of structure so that you can directly use the functions get_layer_activations instead of having to do CEBRA_Lens.activations.get_layer_activations
-from .activations import *
-from .transform import *
-from .quantification import *
-from .plotting import *
-from .utils_allen import *
-
-# selects what files can be imported when doing from CEBRA_Lens import * --> keep env clean
-# __all__ = ['get_layer_activations']
