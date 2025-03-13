32d7e45e10983c14b4cbbf38e1cde7cff361cff3
Eloise Habek <eloise.habek@epfl.ch>
2025-03-13 16:37:13 +0100

    Work on decodeing.py

--- cebra_lens/quantification/base.py
+++ cebra_lens/quantification/base.py
@@ -12,11 +12,11 @@ class _BaseMetric:
 
 class _MultiMetric(_BaseMetric):
 
-    def compute(self, activations_dict, *args, **kwargs):
+    def compute(self, data_dict, *args, **kwargs):
         """Computes metrics for multiple activation layers."""
         result_dict = {}
-        for model_label, activations in activations_dict.items():
-            result_dict[model_label] = []
-            for activation in tqdm(activations, desc=f"Processing {model_label}"):
-                result_dict[model_label].append(activation.compute(*args, **kwargs))
+        for label, values in data_dict.items():
+            result_dict[label] = []
+            for value in tqdm(values, desc=f"Processing {label}"):
+                result_dict[label].append(value.compute(*args, **kwargs))
         return result_dict
--- cebra_lens/quantification/decoding.py
+++ cebra_lens/quantification/decoding.py
@@ -4,117 +4,222 @@ import numpy as np
 from ..utils_allen import decoding_frames
 from ..utils_hpc import decoding_pos_dir
 from ..activations import process_activations, get_activations_model
+from .base import _BaseMetric, _MultiMetric
+from tqdm import tqdm
 
 
-def decode_model(
-    model: cebra.integrations.sklearn.cebra.CEBRA,
-    train_data: torch.Tensor,
-    train_label: np.ndarray,
-    test_data: torch.Tensor,
-    test_label: np.ndarray,
-    session_id: int = -1,
-    dataset_label: str = "visual",
-) -> np.ndarray:
-    """
-    Decodes a single model.
+class MultiDecoding(_MultiMetric):
+    def __init__(self, models_dict: dict):
+        self.models_dict = models_dict
+        self.data = self._transform()
 
-    Parameters:
-    -----------
-    model : cebra.integrations.sklearn.cebra.CEBRA
-        The CEBRA model that will be used to transform the data (either multi-session or single-session model for now).
-    train_data : torch.Tensor
-        The training data used for model transformation.
-    train_label : np.ndarray
-        The true labels corresponding to the training data.
-    test_data : torch.Tensor
-        The validation data used for testing the model.
-    test_label : np.ndarray
-        The true labels corresponding to the validation data.
-    session_id : int, optional
-        The session ID for multi-session models. For single-session no need to input it.
-    dataset_label : str, optional
-        The type of dataset being used for decoding (default is "visual").
+    def _transform(self):
+        result_dict = {}
+        for key, models in self.models_dict.items():
+            result_dict[key] = []
+            for model in tqdm(models, desc=f"Processing {key}"):
+                result_dict[key].append(Decoding(model))
+        return result_dict
 
-    Returns:
-    --------
-    np.ndarray : Array containing the results. Has different structure depending on the dataset used: e.g. 1D array of structure test_score, pos_test_err, pos_test_score for HPC dataset.
-    """
+    def compute(self, *args, **kwargs):
+        return super().compute(models_dict=self.data, *args, **kwargs)
 
-    if model.solver_name_ == "multi-session":
 
-        embedding_train = model.transform(train_data, session_id)
-        embedding_test = model.transform(test_data, session_id)
+class Decoding(_BaseMetric):
+    def __init__(self, model):
+        self.model = model
 
-    elif model.solver_name_ == "single-session":
+    def _decoding_function_selection(
+        # figure out what to do about the arguments and parameters
+        embedding_train: np.ndarray,
+        label_train: np.ndarray,
+        embedding_test: np.ndarray,
+        label_test: np.ndarray,
+        dataset_label: str = "visual",
+    ):
+        """
+        Decodes a model by choosing the appropriate function.
 
-        embedding_train = model.transform(train_data)
-        embedding_test = model.transform(test_data)
+        Parameters:
+        -----------
+        embedding_train : np.ndarray
+            The part of the output embedding to use as training for the decoding.
+        train_label : np.ndarray
+            The true labels corresponding to the training data.
+        embedding_test : np.ndarray
+            The part of the output embedding to use as testing for the decoding.
+        test_label : np.ndarray
+            The true labels corresponding to the validation data.
+        dataset_label : str, optional
+            The type of dataset being used for decoding (default is "visual").
 
-    else:
-        raise NotImplementedError(
-            f"Solver {model.solver_name_} is not yet implemented."
+        Returns:
+        --------
+        np.ndarray : Array containing the results. Has different structure depending on the dataset used: e.g. 1D array of structure test_score, pos_test_err, pos_test_score for HPC dataset.
+        """
+        if (
+            embedding_train.shape[0] < embedding_train.shape[1]
+        ):  # should be samples X neurons
+            embedding_train = embedding_train.T
+        if (
+            embedding_test.shape[0] < embedding_test.shape[1]
+        ):  # should be samples X neurons
+            embedding_test = embedding_test.T
+
+        if dataset_label == "visual":
+
+            results = decoding_frames(
+                embedding_train=embedding_train,
+                label_train=label_train,
+                embedding_test=embedding_test,
+                label_test=label_test,
+            )
+        elif dataset_label == "HPC":
+            results = decoding_pos_dir(
+                embedding_train=embedding_train,
+                label_train=label_train,
+                embedding_test=embedding_test,
+                label_test=label_test,
+            )
+        else:
+            raise NotImplementedError(
+                f"Decoding not implemented for {dataset_label}. Please use 'visual' or 'HPC'."
+            )
+        return results
+
+    def compute(
+        self,
+        train_data: torch.Tensor,
+        train_label: np.ndarray,
+        test_data: torch.Tensor,
+        test_label: np.ndarray,
+        session_id: int,
+        dataset_label: str = "visual",
+        layer_type: str = "conv",
+    ):
+        """
+        Decode neural data by layer using a given CEBRA model.
+
+        Parameters:
+        ----------
+        model : cebra.integrations.sklearn.cebra.CEBRA
+            The CEBRA model that will be used to transform the data (either multi-session or single-session model for now).
+        train_data : torch.Tensor
+            The training data used for model transformation.
+        train_label : np.ndarray
+            The true labels corresponding to the training data.
+        test_data : torch.Tensor
+            The validation data used for testing the model.
+        test_label : np.ndarray
+            The true labels corresponding to the validation data.
+        session_id : int, optional
+            The session ID for multi-session models. For single-session no need to input it.
+        dataset_label : str, optional
+            The type of dataset being used for decoding (default is "visual").
+        layer_type : str, optional
+            The type of layer to extract activations from. Defaults to 'conv'.
+
+        Returns:
+        -------
+        np.ndarray
+            A numpy array containing the decoding results for each layer and the neural input baseline.
+        """
+
+        activations_train = get_activations_model(
+            model=self.model,
+            data=train_data,
+            name=self.model.solver_name_,
+            session_id=session_id,
+            layer_type=layer_type,
         )
 
-    results = _decoding_function_selection(
-        embedding_train, train_label, embedding_test, test_label, dataset_label
-    )
-    return np.array(results)
-
-
-def _decoding_function_selection(
-    embedding_train: np.ndarray,
-    label_train: np.ndarray,
-    embedding_test: np.ndarray,
-    label_test: np.ndarray,
-    dataset_label: str = "visual",
-):
-    """
-    Decodes a model by choosing the appropriate function.
-
-    Parameters:
-    -----------
-    embedding_train : np.ndarray
-        The part of the output embedding to use as training for the decoding.
-    train_label : np.ndarray
-        The true labels corresponding to the training data.
-    embedding_test : np.ndarray
-        The part of the output embedding to use as testing for the decoding.
-    test_label : np.ndarray
-        The true labels corresponding to the validation data.
-    dataset_label : str, optional
-        The type of dataset being used for decoding (default is "visual").
-
-    Returns:
-    --------
-    np.ndarray : Array containing the results. Has different structure depending on the dataset used: e.g. 1D array of structure test_score, pos_test_err, pos_test_score for HPC dataset.
-    """
-    if (
-        embedding_train.shape[0] < embedding_train.shape[1]
-    ):  # should be samples X neurons
-        embedding_train = embedding_train.T
-    if embedding_test.shape[0] < embedding_test.shape[1]:  # should be samples X neurons
-        embedding_test = embedding_test.T
-
-    if dataset_label == "visual":
-
-        results = decoding_frames(
-            embedding_train=embedding_train,
-            label_train=label_train,
-            embedding_test=embedding_test,
-            label_test=label_test,
+        activations_test = get_activations_model(
+            model=self.model,
+            data=test_data,
+            name=self.model.solver_name_,
+            session_id=session_id,
+            layer_type=layer_type,
         )
-    elif dataset_label == "HPC":
-        results = decoding_pos_dir(
-            embedding_train=embedding_train,
-            label_train=label_train,
-            embedding_test=embedding_test,
-            label_test=label_test,
+
+        num_layers = len(activations_train)
+
+        if dataset_label in ["HPC", "visual"]:
+            results = np.zeros((num_layers + 1, 3))
+        else:
+            raise NotImplementedError(
+                f"Decoding not implemented for {dataset_label}. Please use 'visual' or 'HPC'."
+            )
+        keys = list(activations_train.keys())
+        for i in range(num_layers + 1):
+
+            if i == 0:
+                results[i, :] = (
+                    self._decoding_function_selection()
+                )  # neural input baseline
+            else:
+                results[i, :] = self._decoding_function_selection(
+                    activations_train[keys[i - 1]],
+                    train_label,
+                    activations_test[keys[i - 1]],
+                    test_label,
+                    dataset_label,
+                )  # layer decoding
+
+        return results
+
+    def decode(
+        self,
+        train_data: torch.Tensor,
+        train_label: np.ndarray,
+        test_data: torch.Tensor,
+        test_label: np.ndarray,
+        session_id: int = -1,
+        dataset_label: str = "visual",
+    ) -> np.ndarray:
+        """
+        Decodes a single model.
+
+        Parameters:
+        -----------
+        model : cebra.integrations.sklearn.cebra.CEBRA
+            The CEBRA model that will be used to transform the data (either multi-session or single-session model for now).
+        train_data : torch.Tensor
+            The training data used for model transformation.
+        train_label : np.ndarray
+            The true labels corresponding to the training data.
+        test_data : torch.Tensor
+            The validation data used for testing the model.
+        test_label : np.ndarray
+            The true labels corresponding to the validation data.
+        session_id : int, optional
+            The session ID for multi-session models. For single-session no need to input it.
+        dataset_label : str, optional
+            The type of dataset being used for decoding (default is "visual").
+
+        Returns:
+        --------
+        np.ndarray : Array containing the results. Has different structure depending on the dataset used: e.g. 1D array of structure test_score, pos_test_err, pos_test_score for HPC dataset.
+        """
+
+        if self.model.solver_name_ == "multi-session":
+
+            embedding_train = self.model.transform(train_data, session_id)
+            embedding_test = self.model.transform(test_data, session_id)
+
+        elif self.model.solver_name_ == "single-session":
+
+            embedding_train = self.model.transform(train_data)
+            embedding_test = self.model.transform(test_data)
+
+        else:
+            raise NotImplementedError(
+                f"Solver {self.model.solver_name_} is not yet implemented."
+            )
+
+        results = self._decoding_function_selection(
+            embedding_train, train_label, embedding_test, test_label, dataset_label
         )
-    else:
-        raise NotImplementedError(
-            f"Decoding not implemented for {dataset_label}. Please use 'visual' or 'HPC'."
-        )
-    return results
+        return np.array(results)
 
 
 def decode_models(
@@ -172,146 +277,4 @@ def decode_models(
 
         results_dict[key] = results
 
-    return results_dict
-
-
-def decode_layer_model(
-    model: cebra.integrations.sklearn.cebra.CEBRA,
-    train_data: torch.Tensor,
-    train_label: np.ndarray,
-    test_data: torch.Tensor,
-    test_label: np.ndarray,
-    session_id: int,
-    dataset_label: str = "visual",
-    layer_type: str = "conv",
-):
-    """
-    Decode neural data by layer using a given CEBRA model.
-
-    Parameters:
-    ----------
-    model : cebra.integrations.sklearn.cebra.CEBRA
-        The CEBRA model that will be used to transform the data (either multi-session or single-session model for now).
-    train_data : torch.Tensor
-        The training data used for model transformation.
-    train_label : np.ndarray
-        The true labels corresponding to the training data.
-    test_data : torch.Tensor
-        The validation data used for testing the model.
-    test_label : np.ndarray
-        The true labels corresponding to the validation data.
-    session_id : int, optional
-        The session ID for multi-session models. For single-session no need to input it.
-    dataset_label : str, optional
-        The type of dataset being used for decoding (default is "visual").
-    layer_type : str, optional
-        The type of layer to extract activations from. Defaults to 'conv'.
-
-    Returns:
-    -------
-    np.ndarray
-        A numpy array containing the decoding results for each layer and the neural input baseline.
-    """
-
-    activations_train = get_activations_model(
-        model=model,
-        data=train_data,
-        name=model.solver_name_,
-        session_id=session_id,
-        layer_type=layer_type,
-    )
-
-    activations_test = get_activations_model(
-        model=model,
-        data=test_data,
-        name=model.solver_name_,
-        session_id=session_id,
-        layer_type=layer_type,
-    )
-
-    num_layers = len(activations_train)
-
-    if dataset_label in ["HPC", "visual"]:
-        results = np.zeros((num_layers + 1, 3))
-    else:
-        raise NotImplementedError(
-            f"Decoding not implemented for {dataset_label}. Please use 'visual' or 'HPC'."
-        )
-    keys = list(activations_train.keys())
-    for i in range(num_layers + 1):
-
-        if i == 0:
-            results[i, :] = _decoding_function_selection(
-                train_data, train_label, test_data, test_label, dataset_label
-            )  # neural input baseline
-        else:
-            results[i, :] = _decoding_function_selection(
-                activations_train[keys[i - 1]],
-                train_label,
-                activations_test[keys[i - 1]],
-                test_label,
-                dataset_label,
-            )  # layer decoding
-
-    return results
-
-
-def decode_layer_models(
-    models_dict: dict,
-    train_data: torch.Tensor,
-    train_label: np.ndarray,
-    test_data: torch.Tensor,
-    test_label: np.ndarray,
-    session_id: int = -1,
-    dataset_label: str = "visual",
-    layer_type: str = "conv",
-):
-    """
-    Decode neural data by layer using a given CEBRA models.
-
-    Parameters:
-    ----------
-    models_dict : dict
-        Dictionnary containing the CEBRA Models loaded by lens.model.model_loader().
-    train_data : torch.Tensor
-        The training data used for model transformation.
-    train_label : np.ndarray
-        The true labels corresponding to the training data.
-    test_data : torch.Tensor
-        The validation data used for testing the model.
-    test_label : np.ndarray
-        The true labels corresponding to the validation data.
-    session_id : int, optional
-        The session ID for multi-session models. For single-session no need to input it.
-    dataset_label : str, optional
-        The type of dataset being used for decoding (default is "visual").
-    layer_type : str, optional
-        The type of layer to extract activations from. Defaults to 'conv'.
-
-    Returns:
-    -------
-    dict
-        A result dictionnary containing the results of decoding for each layer for each model inside each category:
-        e.g. {category: [2darray_model1_results, 2darray:model2_results, ...]}
-    """
-
-    results_dict = {}
-    for key, models in models_dict.items():
-        results_list = []
-
-        for model in models:
-            results_list.append(
-                decode_layer_model(
-                    model=model,
-                    train_data=train_data,
-                    train_label=train_label,
-                    test_data=test_data,
-                    test_label=test_label,
-                    session_id=session_id,
-                    dataset_label=dataset_label,
-                    layer_type=layer_type,
-                )
-            )
-
-        results_dict[key] = results_list
     return results_dict
