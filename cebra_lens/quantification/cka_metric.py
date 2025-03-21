e07ef1c969847b806e11358d3849d0ed3e62067c
Eloise Habek <eloise.habek@epfl.ch>
2025-03-21 14:42:11 +0100

    CKA fixes

--- cebra_lens/quantification/cka_metric.py
+++ cebra_lens/quantification/cka_metric.py
@@ -2,90 +2,19 @@
 
 from tqdm import tqdm
 import numpy as np
+from helper import *
 
+class ComparisonCKA:
+    def __init__(self,comparison):
 
-class CKA:
-
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    ################################################################
-    ###### Taken from https://github.com/amathislab/DeepDraw #######
-    ################################################################
-
-    def _cka(self, gram_x, gram_y, debiased=False):
-        """Compute CKA.
-
-        Args:
-        gram_x: A num_examples x num_examples Gram matrix.
-        gram_y: A num_examples y num_examples Gram matrix.
-        debiased: Use unbiased estimator of HSIC. CKA may still be biased.
-
-        Returns:
-        The value of CKA between X and Y.
-        """
-        gram_x = self._center_gram(gram_x, unbiased=debiased)
-        gram_y = self._center_gram(gram_y, unbiased=debiased)
-
-        # Note: To obtain HSIC, this should be divided by (n-1)**2 (biased variant) or
-        # n*(n-3) (unbiased variant), but this cancels for CKA.
-        scaled_hsic = gram_x.ravel().dot(gram_y.ravel())
-
-        normalization_x = np.linalg.norm(gram_x)
-        normalization_y = np.linalg.norm(gram_y)
-        return scaled_hsic / (normalization_x * normalization_y)
-
-    def _center_gram(self,gram, unbiased=False):
-        """Center a symmetric Gram matrix.
-
-        This is equvialent to centering the (possibly infinite-dimensional) features
-        induced by the kernel before computing the Gram matrix.
-
-        Args:
-        gram: A num_examples x num_examples symmetric matrix.
-        unbiased: Whether to adjust the Gram matrix in order to compute an unbiased
-            estimate of HSIC. Note that this estimator may be negative.
-
-        Returns:
-        A symmetric matrix with centered columns and rows.
-        """
-        if not np.allclose(gram, gram.T):
-            raise ValueError("Input must be a symmetric matrix.")
-        gram = gram.copy()
-
-        if unbiased:
-            # This formulation of the U-statistic, from Szekely, G. J., & Rizzo, M.
-            # L. (2014). Partial distance correlation with methods for dissimilarities.
-            # The Annals of Statistics, 42(6), 2382-2412, seems to be more numerically
-            # stable than the alternative from Song et al. (2007).
-            n = gram.shape[0]
-            np.fill_diagonal(gram, 0)
-            means = np.sum(gram, 0, dtype=np.float64) / (n - 2)
-            means -= np.sum(means) / (2 * (n - 1))
-            gram -= means[:, None]
-            gram -= means[None, :]
-            np.fill_diagonal(gram, 0)
-        else:
-            means = np.mean(gram, 0, dtype=np.float64)
-            means -= np.mean(means) / 2
-            gram -= means[:, None]
-            gram -= means[None, :]
-
-        return gram
-
-    def _gram_linear(self,x):
-        """Compute Gram (kernel) matrix for a linear kernel.
-
-        Args:
-        x: A num_examples x num_features matrix of features.
-
-        Returns:
-        A num_examples x num_examples Gram matrix of examples.
-        """
-        return x.dot(x.T)
-
-    def compute(self) -> np.ndarray:
+        if not isinstance(comparison, tuple):
+            raise ValueError(
+                f"A comparison must be a tuple. Comparison is of type: {type(comparison)}."
+            )
+        self.comparisonX = comparison[0]
+        self.comparisonY = comparison[1]
+    
+    def _compute(self,embeddings_1: list, embeddings_2: list) -> np.ndarray:
         """
         Compute the Centered Kernel Alignment (CKA) between two sets of embeddings for each layer.
         This function calculates the CKA score between corresponding layers of two sets of embeddings,
@@ -104,44 +33,31 @@ class CKA:
             A one-row array containing the CKA values for each layer.
         """
 
-        # same number of layers and same shape across layers
-        if len(self.x) != len(self.y):
+        if len(embeddings_1) != len(embeddings_2):
             raise ValueError(
                 "The number of layers in embeddings_1 and embeddings_2 must be the same."
             )
-        for i in range(len(self.x)):
-            if self.x[i].shape != self.y[i].shape:
+        for i in range(len(embeddings_1)):
+            if embeddings_1[i].shape != embeddings_2[i].shape:
                 raise ValueError(
                     f"The shape of layer {i} in embeddings_1 and embeddings_2 must be the same."
                 )
 
-        cka_matrix = np.zeros((1, len(self.x)))
-        for i in range(len(self.x)):
-            cka_matrix[0, i] = self._cka(
-                self._gram_linear(self.x[i].T),
-                self._gram_linear(self.y[i].T),
+        cka_matrix = np.zeros((1, len(embeddings_1)))
+        for i in range(len(embeddings_1)):
+            cka_matrix[0, i] = cka(
+                gram_linear(embeddings_1[i].T),
+                gram_linear(embeddings_2[i].T),
             )
         return cka_matrix
-
-
-class MultiCKA:
-    def __init__(self,comparison):
-
-        if not isinstance(comparison, tuple):
-            raise ValueError(
-                f"A comparison must be a tuple. Comparison is of type: {type(comparison)}."
-            )
-        self.comparisonX = comparison[0]
-        self.comparisonY = comparison[1]
 
     def _compute_single(self, embeddings_1, embeddings_2, flag=False):
         self.cka_matrix = np.zeros((len(embeddings_1), len(embeddings_1[0])))
         for j in tqdm(range(len(embeddings_1))):
             if flag:
-                cka = CKA(embeddings_1[j], embeddings_2[j])
+                self.cka_matrix[j, :] = self._compute(embeddings_1[j], embeddings_2[j])
             else:
-                cka = CKA(embeddings_1[j], embeddings_2)
-            self.cka_matrix[j, :] = cka.compute()
+                self.cka_matrix[j, :] = self._compute(embeddings_1[j], embeddings_2)
 
     def compute(self, activations_dict):
         """
