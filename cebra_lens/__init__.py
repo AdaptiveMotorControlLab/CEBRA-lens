ba61d5ce48ee0c76dbcd0c4cd904aed0e3f258e1
Eloise Habek <eloise.habek@epfl.ch>
2025-05-01 16:06:38 +0200

    Bug fix

--- cebra_lens/__init__.py
+++ cebra_lens/__init__.py
@@ -4,7 +4,7 @@ from .quantification import *
 from .matplotlib import *
 from .utils_allen import *
 from .utils_hpc import *
-from .model import *
+from .lens import *
 
 # selects what files can be imported when doing from CEBRA_Lens import * --> keep env clean
 # __all__ = ['get_layer_activations']
